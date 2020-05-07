94cf6652bcd9126eb0a4ed6a68641d6d13a23f77
Michael Timbrook <timbrook@MacBook-Pro.localdomain>
2020-05-06 23:12:25 -0700

    I think this will do it

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ ADD https://get.helm.sh/helm-v3.2.0-linux-386.tar.gz ./helm.tar.gz
 RUN tar -xvf ./helm.tar.gz
 RUN mv ./linux-386/helm /usr/bin/
 
-RUN apk add --no-cache python py-pip py-setuptools ca-certificates libmagic curl
+RUN apk add --no-cache python py-pip py-setuptools ca-certificates libmagic curl jq
 RUN pip install python-dateutil python-magic
 
 RUN S3CMD_CURRENT_VERSION=`curl -fs https://api.github.com/repos/s3tools/s3cmd/releases/latest | grep tag_name | sed -E 's/.*"v?([0-9\.]+).*/\1/g'` \
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,31 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-ls
-pwd
-helm package .
+set +x
 
-s3cmd --host sfo2.digitaloceanspaces.com ls s3://helm-charts
+S3CMD="s3cmd --host-bucket='sfo2.digitaloceanspaces.com' --host sfo2.digitaloceanspaces.com"
+
+publish() {
+    $S3CMD get s3://helm-charts/index.yaml
+    helm repo index --merge index.yaml .
+    PACKAGE=$(ls *.tgz)
+    $S3CMD put -P ./$PACKAGE s3://helm-charts/$PACKAGE
+    $S3CMD put -P ./index.yaml s3://helm-charts/index.yaml
+}
+
+helm repo add personal https://helm-charts.sfo2.digitaloceanspaces.com/
+
+# The following only works once we're publish at least once
+VERSION=$(helm inspect chart personal/service | grep "version:" | cut -d " " -f 2)
+CURRENT_VERSION=$(cat ./Chart.yaml | grep "version:" | cut -d " " -f 2)
+if [[ "$VERSION" == "$CURRENT_VERSION" ]]
+then
+    VERSION=$(helm inspect chart personal/service | grep "version:" | cut -d " " -f 2)
+    helm package --version $VERSION-cont .
+else
+    helm package .
+fi
+
+publish;
 
 time=$(date)
 echo "::set-output name=time::$time"
